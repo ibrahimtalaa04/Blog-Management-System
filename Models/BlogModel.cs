198dd5281963eaf1b88c9440c059296090db2a7a
ibrahimtalaa04 <ibrahim6tal@gmail.com>
2023-06-14 13:24:02 +0300

    add display annotation

--- Models/BlogModel.cs
+++ Models/BlogModel.cs
@@ -8,23 +8,29 @@
 
         [StringLength(100,ErrorMessage ="Title must be less than 100 char!")]
         [Required(ErrorMessage ="Title is required!")]
+        [Display(Name ="Title")]
         public string? title { get; set; }
 
 
         [DataType(DataType.MultilineText)]
         [Required(ErrorMessage ="Content is required!")]
+        [Display(Name = "Content")]
         public string? content { get; set; }
         
         [Required(ErrorMessage ="Category is required!")]
+        [Display(Name = "Category")]
         public string? Category { get; set; }
 
+        [Display(Name = "Date Added")]
         public DateTime? dateAdded { get; set; }= DateTime.Now;
 
 
         [NotMapped]
+        [Display(Name = "Image")]
         public IFormFile? ImageFile { get; set; }
 
         [StringLength(200)]
+        [Display(Name = "Image Name")]
         public string? imagePath { get; set; } = "defaultblog.jpg";
 
         [Required]
