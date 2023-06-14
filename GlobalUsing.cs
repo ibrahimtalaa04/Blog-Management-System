8506136c37f524c7204d35d034088214b4279d81
ibrahimtalaa04 <ibrahim6tal@gmail.com>
2023-06-14 10:35:57 +0300

    add a blog model

--- GlobalUsing.cs
+++ GlobalUsing.cs
@@ -1,4 +1,9 @@
-﻿namespace BlogManagementSystem
+﻿global using System.ComponentModel.DataAnnotations;
+global using System.ComponentModel.DataAnnotations.Schema;
+
+
+
+namespace BlogManagementSystem
 {
     public class GlobalUsing
     {
---
+++ Models/BlogModel.cs
@@ -0,0 +1,30 @@
+﻿namespace BlogManagementSystem.Models
+{
+    public class BlogModel
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [StringLength(100)]
+        [Required]
+        public string? title { get; set; }
+
+
+        [DataType(DataType.MultilineText)]
+        [Required]
+        public string? content { get; set; }
+
+        public DateTime? dateAdded { get; set; }= DateTime.Now;
+
+        [StringLength(200)]
+
+        public string? imagePath { get; set; }
+
+        [Required]
+        public string? UserId { get; set; }
+
+        [ForeignKey("UserId")]
+        public UserModel? UserBlog { get; set; }
+
+    }
+}
