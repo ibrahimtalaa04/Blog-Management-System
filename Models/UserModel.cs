7b142c7313a0f02aa715901b5982dadcdf365615
ibrahimtalaa04 <ibrahim6tal@gmail.com>
2023-06-14 10:41:30 +0300

    update the annotations

--- Models/UserModel.cs
+++ Models/UserModel.cs
@@ -7,19 +7,25 @@ namespace BlogManagementSystem.Models
         [Key]
         public int Id { get; set; }
 
-        [StringLength(50)]
+        [StringLength(50,ErrorMessage ="First Name must be less than 50 char !")]
+        [Required(ErrorMessage ="First Name is required !")]
         public string? firstName { get; set; }
 
-        [StringLength(50)]
+        [StringLength(50,ErrorMessage ="Last Name must be less than 50 char!")]
+        [Required(ErrorMessage ="Last name is required !")]
         public string? lastName { get; set; }
 
-        [StringLength(80)]
+        [StringLength(80,ErrorMessage ="Email must be less than 50 char!")]
+        [Required(ErrorMessage ="Email is required !")]
         public string? email { get; set; }
-        
+
+        [Required(ErrorMessage ="Password is required !")]
+        [StringLength(200)]
         public string? password { get; set; }
 
         [StringLength(200)]
         public string? imagePath { get; set; }
+
         public DateTime dateJoined { get; set; }=DateTime.Now;
 
     }
