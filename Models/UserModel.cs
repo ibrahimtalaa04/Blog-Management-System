a2ad6d4b5cba79a31185c7485d163358306b5847
ibrahimtalaa04 <ibrahim6tal@gmail.com>
2023-06-14 10:29:44 +0300

    add user model

---
+++ Models/UserModel.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BlogManagementSystem.Models
+{
+    public class UserModel
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [StringLength(50)]
+        public string firstName { get; set; }
+
+        [StringLength(50)]
+        public string lastName { get; set; }
+
+        [StringLength(80)]
+        public string email { get; set; }
+        
+        public string password { get; set; }
+
+        [StringLength(200)]
+        public string imagePath { get; set; }
+        public DateTime dateJoined { get; set; }=DateTime.Now;
+
+    }
+}
