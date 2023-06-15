3626a45cc81218f599c3a6a7d481d304386c8bdc
ibrahimtalaa04 <ibrahim6tal@gmail.com>
2023-06-15 11:58:28 +0300

    classes for pagination

---
+++ ViewModels/BlogQueryParamters.cs
@@ -0,0 +1,24 @@
+﻿namespace BlogManagementSystem.ViewModels
+{
+    public class BlogQueryParamters
+    {
+        const int _maxSize = 100;
+        int _size = 20;
+
+        public int CurPage { get; set; } = 1;
+
+        public int Size
+        {
+            get
+            {
+                return _size;
+            }
+            set
+            {
+                _size = Math.Min(_maxSize, value);
+            }
+        }
+
+        public string SearchTearm { get; set; } = string.Empty;
+    }
+}
---
+++ ViewModels/QueryPageResult.cs
@@ -0,0 +1,12 @@
+﻿namespace BlogManagementSystem.ViewModels
+{
+    public class QueryPageResult
+    {
+        public int TotalCount { get; set; } = 0;
+        public int TotalPages { get; set; } = 1;
+        public int? PreviousPage { get; set; }
+        public int? NextPage { get; set; }
+        public int FirstRowOnPage { get; set; }
+        public int LastRowOnPage { get; set; }
+    }
+}
