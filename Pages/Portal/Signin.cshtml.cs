a2ea14f8cf1872adc847f6865232675ee044ff32
ibrahimtalaa04 <ibrahim6tal@gmail.com>
2023-06-15 14:39:42 +0300

    Complete sign in

--- Pages/Portal/Signin.cshtml.cs
+++ Pages/Portal/Signin.cshtml.cs
@@ -1,3 +1,4 @@
+using BlogManagementSystem.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -5,8 +6,44 @@ namespace BlogManagementSystem.Pages.Portal
 {
     public class SigninModel : PageModel
     {
+        public SigninModel()
+        {
+            
+        }
+
+        [BindProperty]
+        public SigninViewModel signinViewModel { get; set; }
+
         public void OnGet()
         {
+        }
+
+
+        public IActionResult OnPost()
+        {
+            if(!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            if (UserRepository.GetByEmail(signinViewModel.Email) == null)
+            {
+                ModelState.AddModelError("signinViewModel.Email", "Email is not exist!");
+                return Page();
+            }
+
+            UserModel? userModel = UserRepository.GetByEmail(signinViewModel.Email);
+
+            if (userModel != null)
+            {
+                if(userModel.password!=signinViewModel.Password)
+                {
+                    ModelState.AddModelError("signinViewModel.Password", "Wrong Password !");
+                    return Page();
+                }
+            }
+
+            return RedirectToPage("/index");
         }
     }
 }
