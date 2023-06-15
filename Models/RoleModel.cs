607592889e3586d447c38f28016d51198450925e
ibrahimtalaa04 <ibrahim6tal@gmail.com>
2023-06-15 15:19:14 +0300

    add role model

---
+++ Models/RoleModel.cs
@@ -0,0 +1,14 @@
+﻿namespace BlogManagementSystem.Models
+{
+    public class RoleModel
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.None)]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage ="Role Name is required !")]
+        [StringLength(100,ErrorMessage ="Role name must be less than 100")]
+        public string? Role { get; set; }
+
+    }
+}
