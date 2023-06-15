66b1e571a47a0e4c44f65f6b7c00705b97ed6363
ibrahimtalaa04 <ibrahim6tal@gmail.com>
2023-06-15 14:43:26 +0300

    add a sign in view model

---
+++ ViewModels/SigninViewModel.cs
@@ -0,0 +1,16 @@
+﻿namespace BlogManagementSystem.ViewModels
+{
+    public class SigninViewModel
+    {
+        [Required(ErrorMessage = "Your Email is required !")]
+        [Display(Name = "Email")]
+        [EmailAddress(ErrorMessage = "Please enter a valid email address")]
+        public string? Email { get; set; }
+
+        [Required(ErrorMessage = "Your Password is required !")]
+        [Display(Name = "Password")]
+        [DataType(DataType.Password)]
+        [StringLength(100, ErrorMessage = "Enter at least {2} characters", MinimumLength = 6)]
+        public string? Password { get; set; }
+    }
+}
