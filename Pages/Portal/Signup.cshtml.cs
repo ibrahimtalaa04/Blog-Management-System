cbba1d55696511dac09a9f4e3502e752e0eee505
ibrahimtalaa04 <ibrahim6tal@gmail.com>
2023-06-15 12:18:16 +0300

    add sign up page

---
+++ Pages/Portal/Signup.cshtml.cs
@@ -0,0 +1,20 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BlogManagementSystem.Pages.Portal
+{
+    public class SignupModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+
+
+
+
+
+        
+
+
+    }
+}
