a49e8818f6fc653ef1b59d7a616226e583734e94
ibrahimtalaa04 <ibrahim6tal@gmail.com>
2023-06-15 13:52:57 +0300

    hands on sign up

--- Pages/Portal/Signup.cshtml.cs
+++ Pages/Portal/Signup.cshtml.cs
@@ -1,12 +1,37 @@
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.RazorPages;
-
 namespace BlogManagementSystem.Pages.Portal
 {
     public class SignupModel : PageModel
     {
+        public SignupModel()
+        {
+            
+        }
+
+
+        [BindProperty]
+        public SignupViewModel signupViewModel { get; set; }
+
+
+
+
+
+
         public void OnGet()
         {
+
+
+
+        }
+
+        public IActionResult OnPost()
+        {
+            if (!ModelState.IsValid)
+                return Page();
+
+            signupViewModel.imagePath = "userdefault.jpg";
+            UserRepository.Signup(signupViewModel);
+
+            return RedirectToPage("/Index");
         }
 
 
