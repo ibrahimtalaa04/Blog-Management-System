cab84f0a7dc05b1b33ec92f0f7f8ced192d874d0
ibrahimtalaa04 <ibrahim6tal@gmail.com>
2023-06-15 12:18:08 +0300

    add sign in page

---
+++ Pages/Portal/Signin.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BlogManagementSystem.Pages.Portal
+{
+    public class SigninModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
